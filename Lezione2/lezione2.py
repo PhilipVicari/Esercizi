3391954993aa7a7e86250839e123917e7a833b7a
PhilipVicari <philipvicari9@gmail.com>
2024-04-18 19:08:59 +0200

    Risolto esercizio 2-4

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -3,5 +3,14 @@
 #2-3 Personal Message: use a variable to represent a person's name, and print a message to that person. 
 #your message should be simple like this: "Hello Eric, would you like to learn some python today?"
 
-name = "Mario"
-print(f"Hello", (name), ", would you like to learn some python today?")
+
+#name = "Mario"
+#print(f"Hello", (name), ", would you like to learn some python today?")
+
+
+#2-4 Use a variable to represent a person's name, and then print tht person's name in uppercase, lowercase and titlecase
+name : str= "Federico"
+uppername= name.upper()
+lowername= name.lower()
+titlename= name.title()
+print(uppername, lowername, titlename)
