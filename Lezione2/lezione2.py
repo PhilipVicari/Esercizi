75f0bea3eba9e431f2dfffb7d193d6b9f8066dcb
PhilipVicari <philipvicari9@gmail.com>
2024-04-26 17:10:38 +0200

    .

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -144,10 +144,8 @@ print(L)
 #6-1. Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live.
 # You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
 Amico={"first_name":"Damiano", "last_name": "Muni", "age":19, "city":"Rome"}
-print(Amico["first_name"])
-print(Amico["last_name"])
-print(Amico["age"])
-print(Amico["city"])
+print(Amico)
+
 #6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary. 
 #Think of a favorite number for each person, and store each as a value in your dictionary. Print each person’s name and their favorite number. 
 #For even more fun, poll a few friends and get some actual data for your program.
@@ -182,11 +180,12 @@ for keys, values in favorite_places.items():
 
 #6-10. Favorite Numbers: Modify your program from Exercise 6-2 so each person can have more than one favorite number. Then print each person’s name along with their favorite numbers.
 People_num={"Marco":"2, 5, 6, 7", "Lorenzo": "10, 3, 6, 2", "Paolo": "5, 18, 45", "Luca": "78, 23, 44"}
-print(People_num)
+for k, v in People_num.items():
+    print(k, v)
 #6-11. Cities: Make a dictionary called cities. Use the names of three cities as keys in your dictionary. 
 #Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city. 
 #The keys for each city’s dictionary should be something like country, population, and fact. Print the name of each city and all of the information you have stored about it.
-cities={"Roma":, "Bologna", "Venezia"}
+cities={"Roma", "Bologna", "Venezia"}
 information={"Country:" :"Italy", "Population:": "2.800.000", "Country:" :"Italy", "Population:": "388.000", "Country:" :"Italy", "Population:": "260.000"}
 #6-12. Extensions: We’re now working with examples that are complex enough that they can be extended in any number of ways. 
 #Use one of the example programs from this chapter, and extend it by adding new keys and values, changing the context of the program, or improving the formatting of the output.
